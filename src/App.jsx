1d992bfc5fc79064d93b7f8b8753ef49750650a1
CesarLeoni <leoni.cesar.emanuele@gmail.com>
2024-12-14 03:48:33 +0200

    added bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 const App = () => {
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline">
-      Leoni Cesar
-    </h1>
-    </div>
+    <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
+       <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center">
+       </div>
+       <div><h4>Cesar Leon</h4></div>
+    </main>
+
+    
   )
 }
 
