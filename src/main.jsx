9962d501b0318de27cf50050ab816a571aa211b0
Cesar <leoni.cesar.emanuele@gmail.com>
2025-02-04 16:10:49 +0200

    sper ca tailwind va functiona

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import 'tailwindcss/tailwind.css'; // Tailwind CSS should be imported after
-import App from './App.jsx'
-import { gsap } from "gsap";
-
+import './index.css'; // Ensure this file includes @tailwind base, @tailwind components, @tailwind utilities
+import App from './App.jsx';
+import { gsap } from "gsap"; // If you plan to use GSAP in your app
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
