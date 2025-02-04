d06e4e484fb7aed70ccea534777e86c80ba9c928
Cesar <leoni.cesar.emanuele@gmail.com>
2025-02-04 16:12:25 +0200

    sper ca bn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-// tailwind.config.js (v3.x)
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./index.html", // Adjust these paths according to your project structure
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("tailwind-scrollbar-hide"), // Your plugins
-  ],
-};
+  plugins: [require("tailwind-scrollbar-hide"), ],// Your plugins
+}
